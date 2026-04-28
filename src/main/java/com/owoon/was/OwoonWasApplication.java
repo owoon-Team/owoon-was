d2333d5dcaf674fb5a11551099a954c8fdb9789e
eastminnn <oh1334000@gmail.com>
2026-04-28 20:19:17 +0900

    chore : 초기 환경설정 및 .env 기반 설정 추가

---
+++ src/main/java/com/owoon/was/OwoonWasApplication.java
@@ -0,0 +1,13 @@
+package com.owoon.was;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwoonWasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwoonWasApplication.class, args);
+    }
+}
+
